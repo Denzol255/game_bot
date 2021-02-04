6a8ae528da8d67bbc5f336f822138a522687d888
Denzol255 <den-zoloto@mail.ru>
2021-02-05 02:21:44 +0300

    Create: full hard task Fix: basic task

--- js/script.js
+++ js/script.js
@@ -3,31 +3,41 @@
 let isNumber = function (n) {
   return !isNaN(parseInt(n)) && isFinite(n) && n >= 1 && n <= 100;
 };
-
 function guessNumber() {
-  let ourNumber = Math.floor(100 * Math.random()) + 1;
-  console.log(ourNumber);
+  const ourNumber = Math.floor(100 * Math.random()) + 1;
+  // console.log(ourNumber) -- Читы
   let userNumber = prompt("Угадай число от 1 до 100");
+  let count = 9;
   const checkNumber = function () {
-    if (userNumber === null) {
-      alert("Игра окончена!");
-    } else if (!isNumber(userNumber)) {
-      userNumber = prompt("Введите число от 1 до 100!");
-      return checkNumber();
-    } else if (userNumber == ourNumber) {
-      alert("Поздравляю, вы угадали!");
-    } else if (userNumber < ourNumber) {
-      let increaseNumber = function () {
-        userNumber = prompt("Загаданное число больше");
-        return checkNumber();
-      };
-      increaseNumber();
-    } else if (userNumber > ourNumber) {
-      let reduceNumber = function () {
-        userNumber = prompt("Загаданное число меньше");
-        return checkNumber();
-      };
-      reduceNumber();
+    if (count > 0) {
+      if (userNumber === null) {
+        alert("Игра окончена!");
+      } else if (!isNumber(userNumber)) {
+        userNumber = prompt("Введите число от 1 до 100!");
+        checkNumber();
+      } else if (+userNumber === ourNumber) {
+        let choise = confirm("Поздравляю, вы угадали! Хотели бы сыграть еще?");
+        if (choise) {
+          location.reload();
+        }
+      } else if (userNumber < ourNumber) {
+        userNumber = prompt(
+          `Загаданное число больше, осталось попыток ${count}`
+        );
+        count--;
+        checkNumber();
+      } else if (userNumber > ourNumber) {
+        userNumber = prompt(
+          `Загаданное число меньше, осталось попыток ${count}`
+        );
+        count--;
+        checkNumber();
+      }
+    } else {
+      let choise = confirm("Попытки закончились, хотите сыграть еще?");
+      if (choise) {
+        location.reload();
+      }
     }
   };
   checkNumber();
