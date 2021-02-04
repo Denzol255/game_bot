b36b432ae9853bb82671fe30b31de381567e33ee
Denzol255 <den-zoloto@mail.ru>
2021-02-05 02:30:52 +0300

    Fix: delete useless

--- js/script.js
+++ js/script.js
@@ -5,29 +5,23 @@ let isNumber = function (n) {
 };
 
 function guessNumber() {
-  let ourNumber = Math.floor(100 * Math.random()) + 1;
-  console.log(ourNumber);
+  const ourNumber = Math.floor(100 * Math.random()) + 1;
+  // console.log(ourNumber); -- Читы
   let userNumber = prompt("Угадай число от 1 до 100");
   const checkNumber = function () {
     if (userNumber === null) {
       alert("Игра окончена!");
     } else if (!isNumber(userNumber)) {
       userNumber = prompt("Введите число от 1 до 100!");
-      return checkNumber();
-    } else if (userNumber == ourNumber) {
+      checkNumber();
+    } else if (+userNumber === ourNumber) {
       alert("Поздравляю, вы угадали!");
     } else if (userNumber < ourNumber) {
-      let increaseNumber = function () {
-        userNumber = prompt("Загаданное число больше");
-        return checkNumber();
-      };
-      increaseNumber();
+      userNumber = alert("Загаданное число больше");
+      checkNumber();
     } else if (userNumber > ourNumber) {
-      let reduceNumber = function () {
-        userNumber = prompt("Загаданное число меньше");
-        return checkNumber();
-      };
-      reduceNumber();
+      userNumber = alert("Загаданное число меньше");
+      checkNumber();
     }
   };
   checkNumber();
